f8880e92c4d8d20dd79d0b366b77e1936bc3ace3
egaeuni <gaeun3619@gmail.com>
2024-09-13 20:31:32 +0900

    DRF generics

--- example/urls.py
+++ example/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('cbv/book/<int:bid>/', BookAPI.as_view()),
     path('mixins/books/', BooksAPIMixins.as_view()),
     path('mixins/book/<int:bid>/', BookAPIMixins.as_view()),
+    path('generics/books/', BooksAPIGenerics.as_view()),
+    path('generics/book/<int:bid>/', BookAPIGenerics.as_view()),
 ]
