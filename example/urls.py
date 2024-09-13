a1f7e5215b600cf80216a2a632f7abfa0c815347
egaeuni <gaeun3619@gmail.com>
2024-09-13 20:30:14 +0900

    DRF mixins

--- example/urls.py
+++ example/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('fbv/book/<int:bid>/', bookAPI),
     path('cbv/books/', BooksAPI.as_view()),
     path('cbv/book/<int:bid>/', BookAPI.as_view()),
+    path('mixins/books/', BooksAPIMixins.as_view()),
+    path('mixins/book/<int:bid>/', BookAPIMixins.as_view()),
 ]
