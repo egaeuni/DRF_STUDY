36dc2f34965ad3b1701329877427d996b8779bdb
egaeuni <gaeun3619@gmail.com>
2024-09-13 20:26:25 +0900

    DRF Serializer

--- example/urls.py
+++ example/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from .views import HellopAPI
+from .views import *
 
 urlpatterns = [
-    path("hello/", HellopAPI)
+    path("hello/", HellopAPI),
+    path("fbv/books/", booksAPI),
+    path('fbv/book/<int:bid>/', bookAPI),
+    path('cbv/books/', BooksAPI.as_view()),
+    path('cbv/book/<int:bid>/', BookAPI.as_view()),
 ]
